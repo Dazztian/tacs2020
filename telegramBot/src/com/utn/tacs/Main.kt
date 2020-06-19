e64fa0eebd7ae34b85ae01813f5ffea1444d403e
isarasibar <ignaciosarasibar@hendercross.com>
2020-06-19 00:27:03 -0300

    Fix

--- telegramBot/src/com/utn/tacs/Main.kt
+++ telegramBot/src/com/utn/tacs/Main.kt
@@ -9,7 +9,7 @@ import java.io.File
 
 //Map of last important messages separated by each telegram user Id
 val lastImportantMessages = mutableMapOf<Long, PreviousMessageWrapper>()
-lateinit var urlBase :String
+var urlBase :String = "http://localhost:8080/"
 
 fun main() {
     val bot = createBot()
@@ -22,7 +22,7 @@ fun createBot() :Bot = bot {
         throw Exception("No API Key specified for the bot")
     }
 
-    urlBase = this::class.java.getResource("/APIKey.txt").readText()
+    urlBase = this::class.java.getResource("/Base_Url.txt").readText()
     if(urlBase.isBlank()){
         throw Exception("No base url specified for the bot")
     }
