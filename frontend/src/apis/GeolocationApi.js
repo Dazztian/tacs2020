ea5645c8496daf4338350dd39368540bca539bf7
paniaton <lucpanizz@gmail.com>
2020-06-30 13:47:47 -0300

    sin logs

--- frontend/src/apis/GeolocationApi.js
+++ frontend/src/apis/GeolocationApi.js
@@ -12,7 +12,6 @@ async function getCountry(){
       //const res = await Promise.race([promapi1,promapi2])
       const res = await fetch(`https://geocode.xyz/-34.60988,-58.45221?json=1`);
       const data = await res.json();
-      console.log(data)
       return {countryIso: data.prov, countryName: data.country};
       
     } catch(error){
